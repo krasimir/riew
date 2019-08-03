4820203e79ebcb98bf6c42fb0bd80d6624336869
Krasimir Tsonev <krasimir@outset.ws>
2019-08-03 17:02:11 +0300

    Clean up tests

--- src/__tests__/integration.spec.js
+++ src/__tests__/integration.spec.js
@@ -1,10 +1,196 @@
-/* eslint-disable react/prop-types */
+/* eslint-disable react/prop-types, camelcase */
 import React, { useState } from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, act, fireEvent } from '@testing-library/react';
+import { delay, exerciseHTML } from '../__helpers__';
 
-import { routine } from '../index';
+import { routine, System, state, connect, put, take, takeEvery } from '../index';
 
 describe('Given the Rine library', () => {
+  describe('when we want to render children', () => {
+    it('should pass the `children` prop down to our function', () => {
+      const B = ({ children }) => {
+        return <p>{ children }</p>;
+      };
+      const A = routine(render => {
+        render(({ children }) => <B>{ children }</B>);
+      });
+      const { container } = render(<A>Hello world</A>);
+
+      exerciseHTML(container, '<p>Hello world</p>');
+    });
+  });
+  describe('when we use an async function', () => {
+    it('should allow us to render multiple times', async () => {
+      const A = routine(async (render) => {
+        act(() => { render('Hello'); });
+        await delay(20);
+        act(() => { render('world'); });
+      });
+
+      const { queryByText, getByText } = render(<A />);
+
+      expect(getByText('Hello')).toBeDefined();
+      await delay(21);
+      expect(queryByText('Hello')).toBe(null);
+      expect(getByText('world')).toBeDefined();
+    });
+    it('should not try to re-render if the bridge is unmounted', async () => {
+      const spy = jest.spyOn(console, 'error');
+      const A = routine(async (render) => {
+        await delay(20);
+        act(() => { render('world'); });
+      });
+
+      const { unmount } = render(<A/>);
+
+      unmount();
+      await delay(21);
+      expect(spy).not.toBeCalled();
+      spy.mockRestore();
+    });
+  });
+  describe('when reusing the same routine', () => {
+    it('should create a separate controller', () => {
+      const R = routine(function (render) {
+        render(props => <p>{ props.answer }</p>);
+      });
+
+      const { container } = render(<R answer='foo' />);
+      const { container: container2, rerender: rerender2 } = render(<R answer='bar' />);
+
+      exerciseHTML(container, `
+        <p>foo</p>
+      `);
+      exerciseHTML(container2, `
+        <p>bar</p>
+      `);
+      rerender2(<R answer='zoo' />);
+      exerciseHTML(container, `
+        <p>foo</p>
+      `);
+      exerciseHTML(container2, `
+        <p>zoo</p>
+      `);
+    });
+  });
+  describe('when using `take` and `put`', () => {
+    it(`should
+      - pause the routine till the event is fired
+      - allow for only one put`, async () => {
+      const A = routine(async (render) => {
+        act(() => {
+          render(<button onClick={ () => put('xxx', 'bar') }>click me</button>);
+        });
+        const r = await take('xxx');
+
+        expect(r).toEqual('bar');
+        act(() => { render(<p>Yeah</p>); });
+      });
+
+      const { container, getByText } = render(<A />);
+
+      fireEvent.click(getByText('click me'));
+
+      await delay(1);
+      exerciseHTML(container, `
+        <p>Yeah</p>
+      `);
+      expect(System.tasks).toHaveLength(0);
+    });
+  });
+  describe('when using `take` in a fork fashion', () => {
+    it(`should
+      - run the callback after the put call
+      - allow for only one put call`, async () => {
+      const A = routine(async (render) => {
+        act(() => { render(<button onClick={ () => put('foo', 'bar') }>click me</button>); });
+        take('foo', async (r) => {
+          expect(r).toEqual('bar');
+          act(() => { render(<p>Yeah</p>); });
+        });
+      });
+
+      const { container, getByText } = render(<A />);
+
+      fireEvent.click(getByText('click me'));
+
+      exerciseHTML(container, `
+        <p>Yeah</p>
+      `);
+      expect(System.tasks).toHaveLength(0);
+    });
+  });
+  describe('when using `takeEvery` and `put`', () => {
+    it('should fire the callback on every `put`', async () => {
+      const Counter = ({ value, onClick }) => (
+        <React.Fragment>
+          <p>{ value }</p>
+          <button onClick={ onClick }>click me</button>)
+        </React.Fragment>
+      );
+      const A = routine(async (render) => {
+        let counter = 0;
+        let renderCounter = () => act(() => { render(<Counter value={ counter } onClick={ () => put('foo', 2) } />); });
+
+        renderCounter();
+        takeEvery('foo', (payload) => {
+          counter += payload;
+          renderCounter();
+        });
+      });
+
+      const { container, getByText } = render(<A />);
+
+      fireEvent.click(getByText('click me'));
+      exerciseHTML(container.querySelector('p'), '2');
+      fireEvent.click(getByText('click me'));
+      fireEvent.click(getByText('click me'));
+      fireEvent.click(getByText('click me'));
+      exerciseHTML(container.querySelector('p'), '8');
+    });
+  });
+  describe('when using `take` and `put` in different routines', () => {
+    it('should allow the communication between them', async () => {
+      const A = routine(async (render) => {
+        await take('foo');
+        act(() => { render(<p>It works</p>); });
+      });
+      const B = routine(async () => {
+        await delay(10);
+        put('foo');
+      });
+
+      const { container } = render(
+        <React.Fragment>
+          <A />
+          <B />
+        </React.Fragment>
+      );
+
+      await delay(11);
+      exerciseHTML(container, `
+        <p>It works</p>
+      `);
+    });
+  });
+  describe('when we use the `isMounted` method', () => {
+    it('should return the value of the `mounted` flag', async () => {
+      const spy = jest.fn();
+      const A = routine(async (render, { isMounted }) => {
+        spy(isMounted());
+        await delay(10);
+        spy(isMounted());
+      });
+
+      const { unmount } = render(<A />);
+
+      unmount();
+      await delay(11);
+      expect(spy).toHaveBeenCalledTimes(2);
+      expect(spy.mock.calls[0]).toStrictEqual([true]);
+      expect(spy.mock.calls[1]).toStrictEqual([false]);
+    });
+  });
   describe('when using routine routine with a hook', () => {
     it('should keep the hook working', async () => {
       const Input = function () {
