43cfb764719c237a67a3e030f5814445c00ee589
Krasimir Tsonev <krasimir@outset.ws>
2019-08-10 06:25:31 +0300

    Updating the example to the new 0.29.0

--- examples/todomvc/src/index.js
+++ examples/todomvc/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { routine, connect } from 'rine';
+import PropTypes from 'prop-types';
+import { routine, connect, put } from 'rine';
 
 import List from './List';
 import Footer from './Footer';
@@ -18,48 +19,61 @@ import {
   CLEAR_COMPLETED
 } from './constants';
 
-const App = routine(function App({ render, state, put }) {
+const App = ({ list, footer }) => (
+  <React.Fragment>
+    <section className='todoapp'>
+      <header className='header'>
+        <h1>todos</h1>
+        <input
+          className='new-todo'
+          placeholder='What needs to be done?'
+          autoFocus
+          onKeyUp={ e => {
+            if (e.keyCode === ENTER) {
+              put(NEW_TODO, e.target.value);
+              e.target.value = '';
+            }
+          }}/>
+      </header>
+      <section className='main'>
+        <input id='toggle-all' className='toggle-all' type='checkbox' />
+        <label htmlFor='toggle-all'>Mark all as complete</label>
+        { list }
+      </section>
+      { footer }
+    </section>
+  </React.Fragment>
+);
+
+App.propTypes = {
+  list: PropTypes.object.isRequired,
+  footer: PropTypes.object.isRequired
+};
+
+const RoutineComponent = routine(function App({ render, state }) {
   const todos = state(getInitialTodosData(), reducer);
   const filter = state(ALL);
   const ListPartial = connect(List, { todos, filter });
   const FooterPartial = connect(Footer, { todos, filter });
 
   todos.subscribe(saveTodosData);
+  render({
+    list: (
+      <ListPartial
+        onToggle={ index => put(TOGGLE, index) }
+        onDelete={ index => put(DELETE, index) }
+        onEdit={ index => put(EDIT, { index, value: true }) }
+        onUpdate={ (index, label) => put(UPDATE, { index, label }) }
+        onUpdateCancel={ index => put(EDIT, { index, value: false }) } />
+    ),
+    footer: (
+      <FooterPartial
+        all={ () => filter.set(ALL) }
+        active={ () => filter.set(ACTIVE) }
+        completed={ () => filter.set(COMPLETED) }
+        clearCompleted={ () => put(CLEAR_COMPLETED) } />
+    )
+  });
+}, App);
 
-  render(
-    <React.Fragment>
-      <section className='todoapp'>
-        <header className='header'>
-          <h1>todos</h1>
-          <input
-            className='new-todo'
-            placeholder='What needs to be done?'
-            autoFocus
-            onKeyUp={ e => {
-              if (e.keyCode === ENTER) {
-                put(NEW_TODO, e.target.value);
-                e.target.value = '';
-              }
-            }}/>
-        </header>
-        <section className='main'>
-          <input id='toggle-all' className='toggle-all' type='checkbox' />
-          <label htmlFor='toggle-all'>Mark all as complete</label>
-          <ListPartial
-            onToggle={ index => put(TOGGLE, index) }
-            onDelete={ index => put(DELETE, index) }
-            onEdit={ index => put(EDIT, { index, value: true }) }
-            onUpdate={ (index, label) => put(UPDATE, { index, label }) }
-            onUpdateCancel={ index => put(EDIT, { index, value: false }) } />
-        </section>
-        <FooterPartial
-          all={ () => filter.set(ALL) }
-          active={ () => filter.set(ACTIVE) }
-          completed={ () => filter.set(COMPLETED) }
-          clearCompleted={ () => put(CLEAR_COMPLETED) } />
-      </section>
-    </React.Fragment>
-  );
-});
-
-ReactDOM.render(<App />, document.querySelector('#container'));
+ReactDOM.render(<RoutineComponent />, document.querySelector('#container'));
