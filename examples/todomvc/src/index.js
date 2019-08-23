ce1163831cae7a84dd64b8d8f953a1b771215a2e
Krasimir Tsonev <krasimir@outset.ws>
2019-08-23 06:59:02 +0300

    /react path

--- examples/todomvc/src/index.js
+++ examples/todomvc/src/index.js
@@ -1,74 +1,78 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { rine, connect } from 'rine';
+import { state } from 'rine';
+import routine from 'rine/lib/react';
 
-import List from './List';
-import Footer from './Footer';
-import {
-  saveTodosData,
-  toggle,
-  newTodo,
-  editingTodo,
-  updateTodo,
-  deleteTodo,
-  clearCompleted
-} from './Data';
-import {
-  ENTER,
-  ALL,
-  ACTIVE,
-  COMPLETED
-} from './constants';
+console.log(state);
+console.log(routine);
 
-const App = rine(function App({ render, state, todos }) {
-  const filter = state(ALL);
-  const changeFilter = filter.mutation((current, payload) => payload);
+// import List from './List';
+// import Footer from './Footer';
+// import {
+//   saveTodosData,
+//   toggle,
+//   newTodo,
+//   editingTodo,
+//   updateTodo,
+//   deleteTodo,
+//   clearCompleted
+// } from './Data';
+// import {
+//   ENTER,
+//   ALL,
+//   ACTIVE,
+//   COMPLETED
+// } from './constants';
 
-  connect({ todos }, ({ todos }) => {
-    saveTodosData(todos);
-    render({ todos });
-  });
-  connect({ filter }, render);
-  render({ changeFilter });
-}, ({ todos, filter, changeFilter }) => (
-  <React.Fragment>
-    <section className='todoapp'>
-      <header className='header'>
-        <h1>todos</h1>
-        <input
-          className='new-todo'
-          placeholder='What needs to be done?'
-          autoFocus
-          onKeyUp={ e => {
-            if (e.keyCode === ENTER) {
-              newTodo(e.target.value);
-              e.target.value = '';
-            }
-          }}/>
-      </header>
-      <section className='main'>
-        <input id='toggle-all' className='toggle-all' type='checkbox' />
-        <label htmlFor='toggle-all'>Mark all as complete</label>
-        <List
-          todos={ todos }
-          filter={ filter }
-          onToggle={ toggle }
-          onDelete={ deleteTodo }
-          onEdit={ (index) => editingTodo({ index, value: true }) }
-          onUpdate={ (index, label) => updateTodo({ index, label }) }
-          onUpdateCancel={ index => editingTodo({ index, value: false }) } />
-      </section>
-        <Footer
-          todos={ todos }
-          filter={ filter }
-          all={ () => changeFilter(ALL) }
-          active={ () => changeFilter(ACTIVE) }
-          completed={ () => changeFilter(COMPLETED) }
-          clearCompleted={ clearCompleted } />
-    </section>
-  </React.Fragment>
-));
+// const App = rine(function App({ render, state, todos }) {
+//   const filter = state(ALL);
+//   const changeFilter = filter.mutation((current, payload) => payload);
 
-App.inject('todos');
+//   connect({ todos }, ({ todos }) => {
+//     saveTodosData(todos);
+//     render({ todos });
+//   });
+//   connect({ filter }, render);
+//   render({ changeFilter });
+// }, ({ todos, filter, changeFilter }) => (
+//   <React.Fragment>
+//     <section className='todoapp'>
+//       <header className='header'>
+//         <h1>todos</h1>
+//         <input
+//           className='new-todo'
+//           placeholder='What needs to be done?'
+//           autoFocus
+//           onKeyUp={ e => {
+//             if (e.keyCode === ENTER) {
+//               newTodo(e.target.value);
+//               e.target.value = '';
+//             }
+//           }}/>
+//       </header>
+//       <section className='main'>
+//         <input id='toggle-all' className='toggle-all' type='checkbox' />
+//         <label htmlFor='toggle-all'>Mark all as complete</label>
+//         <List
+//           todos={ todos }
+//           filter={ filter }
+//           onToggle={ toggle }
+//           onDelete={ deleteTodo }
+//           onEdit={ (index) => editingTodo({ index, value: true }) }
+//           onUpdate={ (index, label) => updateTodo({ index, label }) }
+//           onUpdateCancel={ index => editingTodo({ index, value: false }) } />
+//       </section>
+//         <Footer
+//           todos={ todos }
+//           filter={ filter }
+//           all={ () => changeFilter(ALL) }
+//           active={ () => changeFilter(ACTIVE) }
+//           completed={ () => changeFilter(COMPLETED) }
+//           clearCompleted={ clearCompleted } />
+//     </section>
+//   </React.Fragment>
+// ));
 
-ReactDOM.render(<App />, document.querySelector('#container'));
+// App.inject('todos');
+
+// ReactDOM.render(<App />, document.querySelector('#container'));
