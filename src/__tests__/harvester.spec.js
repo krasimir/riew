6c31dcac4677a7ec119e31f98994234341810c29
Krasimir Tsonev <krasimir@outset.ws>
2019-09-18 07:10:19 +0300

    Covering the use/register with a unit test

---
+++ src/__tests__/harvester.spec.js
@@ -0,0 +1,17 @@
+import harvester from '../harvester';
+import { use, register } from '../index';
+
+describe('Given the harvester', () => {
+  beforeEach(() => {
+    harvester.reset();
+  });
+  describe('when we register something in there', () => {
+    it('should let us use it later', () => {
+      const myFunc = () => 42;
+
+      register('foo', myFunc);
+      expect(use('foo')()).toBe(42);
+      expect(() => register('foo', 'bar')).toThrowError('An entry with name "foo" already exists.');
+    });
+  });
+});
