eb216bb3904b1effbb41c3fbb369058413120db9
Krasimir Tsonev <krasimir@outset.ws>
2019-12-09 18:14:54 +0200

    Wrapping the pub sub pattern

--- src/__tests__/csp.pubsub.spec.js
+++ src/__tests__/csp.pubsub.spec.js
@@ -1,47 +1,75 @@
-import { chan, sub, unsub, unsubAll } from '../index';
+import { sub, pub, unsub, unsubAll, pubsubReset, deleteTopic } from '../index';
 
 describe('Given a CSP pubsub extension', () => {
-  describe('when we publish to a channel', () => {
+  beforeEach(() => {
+    pubsubReset();
+  });
+  describe('when we publish', () => {
     it('should provide an API for subscribing', () => {
-      const ch = chan();
       const spyA = jest.fn();
       const spyB = jest.fn();
 
-      sub(ch, spyA);
-      sub(ch, spyB);
+      sub('topic', spyA);
+      sub('topic', spyB);
 
-      ch.put('foo');
-      ch.put('bar');
+      pub('topic', 'foo');
+      pub('topic', 'bar');
 
       expect(spyA).toBeCalledWithArgs([ 'foo' ], [ 'bar' ]);
       expect(spyB).toBeCalledWithArgs([ 'foo' ], [ 'bar' ]);
     });
-    it('should provide an API for unsubscribing', () => {
-      const ch = chan();
+    it('should create a dedicated channel for each topic', () => {
       const spyA = jest.fn();
       const spyB = jest.fn();
 
-      sub(ch, spyA);
-      sub(ch, spyB);
+      sub('topicA', spyA);
+      sub('topicB', spyB);
 
-      ch.put('foo');
-      unsub(ch, spyA);
-      ch.put('bar');
+      pub('topicA', 'foo');
+      pub('topicA', 'bar');
+      pub('topicB', 'baz');
+
+      expect(spyA).toBeCalledWithArgs([ 'foo' ], [ 'bar' ]);
+      expect(spyB).toBeCalledWithArgs([ 'baz' ]);
+    });
+    it('should provide an API for unsubscribing', () => {
+      const spyA = jest.fn();
+      const spyB = jest.fn();
+
+      sub('a', spyA);
+      sub('a', spyB);
+
+      pub('a', 'foo');
+      unsub('a', spyA);
+      pub('a', 'bar');
 
       expect(spyA).toBeCalledWithArgs([ 'foo' ]);
       expect(spyB).toBeCalledWithArgs([ 'foo' ], [ 'bar' ]);
     });
     it('should provide an API for unsubscribing all subscribed callbacks', () => {
-      const ch = chan();
       const spyA = jest.fn();
       const spyB = jest.fn();
 
-      sub(ch, spyA);
-      sub(ch, spyB);
+      sub('a', spyA);
+      sub('a', spyB);
 
-      ch.put('foo');
-      unsubAll(ch);
-      ch.put('bar');
+      pub('a', 'foo');
+      unsubAll('a');
+      pub('a', 'bar');
+
+      expect(spyA).toBeCalledWithArgs([ 'foo' ]);
+      expect(spyB).toBeCalledWithArgs([ 'foo' ]);
+    });
+    it('should provide an API for deleting a topic', () => {
+      const spyA = jest.fn();
+      const spyB = jest.fn();
+
+      sub('a', spyA);
+      sub('a', spyB);
+
+      pub('a', 'foo');
+      deleteTopic('a');
+      pub('a', 'bar');
 
       expect(spyA).toBeCalledWithArgs([ 'foo' ]);
       expect(spyB).toBeCalledWithArgs([ 'foo' ]);
--- src/csp/ext/pubsub.js
+++ src/csp/ext/pubsub.js
@@ -1,34 +1,51 @@
 import { chan } from '../channel';
 
 const PubSub = function () {
-  const channels = {};
+  let channels = {};
+  const createChannel = topic => {
+    if (!channels[ topic ]) {
+      const ch = chan();
+      const bus = (channels[ topic ] = {
+        ch,
+        subscribers: []
+      });
+      (function taker() {
+        ch.take(value => {
+          if (value !== chan.CLOSED && value !== chan.ENDED) {
+            bus.subscribers.forEach(s => s(value));
+            taker();
+          }
+        });
+      })();
+    }
+  };
 
   const api = {
-    register(ch) {
-      if (!channels[ ch.id ]) {
-        channels[ ch.id ] = { subscribers: [] };
-        (function taker() {
-          ch.take(value => {
-            if (value !== chan.CLOSED && value !== chan.ENDED) {
-              channels[ ch.id ].subscribers.forEach(s => s(value));
-              taker();
-            }
-          });
-        })();
+    subscribe(topic, callback) {
+      createChannel(topic);
+      channels[ topic ].subscribers.push(callback);
+    },
+    publish(topic, payload, callback) {
+      createChannel(topic);
+      channels[ topic ].ch.put(payload, callback);
+    },
+    unsubscribe(topic, callback) {
+      if (channels[ topic ]) {
+        channels[ topic ].subscribers = channels[ topic ].subscribers.filter(c => c !== callback);
       }
     },
-    subscribe(ch, callback) {
-      api.register(ch);
-      channels[ ch.id ].subscribers.push(callback);
-    },
-    unsubscribe(ch, callback) {
-      if (channels[ ch.id ]) {
-        channels[ ch.id ].subscribers = channels[ ch.id ].subscribers.filter(c => c !== callback);
+    unsubscribeAll(topic) {
+      if (channels[ topic ]) {
+        channels[ topic ].subscribers = [];
       }
     },
-    unsubscribeAll(ch) {
-      if (channels[ ch.id ]) {
-        channels[ ch.id ].subscribers = [];
+    reset() {
+      channels = {};
+    },
+    deleteTopic(topic) {
+      if (channels[ topic ]) {
+        channels[ topic ].ch.close();
+        delete channels[ topic ];
       }
     }
   };
@@ -38,5 +55,8 @@ const PubSub = function () {
 const ps = PubSub();
 
 export const sub = ps.subscribe;
+export const pub = ps.publish;
 export const unsub = ps.unsubscribe;
 export const unsubAll = ps.unsubscribeAll;
+export const pubsubReset = ps.reset;
+export const deleteTopic = ps.deleteTopic;
