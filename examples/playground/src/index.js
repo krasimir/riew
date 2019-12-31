79d77a83e1963395cc9a3c76d6d06f5bd82939c7
Krasimir Tsonev <krasimir@outset.ws>
2019-12-31 10:33:41 +0200

    Updating playground

--- examples/playground/src/index.js
+++ examples/playground/src/index.js
@@ -21,6 +21,7 @@ function B(n) {
 A(10);
 */
 
+/*
 function createChannel() {
   const puts = [];
   const takes = [];
@@ -68,6 +69,45 @@ async function B() {
   setTimeout(() => {
     channel.put(fibonacci(index));
   }, 1000);
+}
+
+A(10);
+B();
+B();
+A(0);
+A(3);
+B();
+*/
+
+const Bus = {
+  _subscribers: {},
+  subscribe(type, callback) {
+    if (!this._subscribers[type]) this._subscribers[type] = [];
+    this._subscribers[type].push(callback);
+  },
+  send(type, ...payload) {
+    if (this._subscribers[type]) {
+      this._subscribers[type].forEach(s => s(...payload));
+    }
+  },
+};
+let t = 0;
+function A(n) {
+  Bus.subscribe('FIBONACCI_NUMBER', (index, number) => {
+    console.log(++t, `Fibonacci number for index ${index} is ${number}`);
+  });
+  Bus.send('CALCULATE_FIBONACCI', n);
+}
+function B() {
+  const fibonacci = num => {
+    if (num <= 1) return 1;
+    return fibonacci(num - 1) + fibonacci(num - 2);
+  };
+  Bus.subscribe('CALCULATE_FIBONACCI', n => {
+    setTimeout(() => {
+      Bus.send('FIBONACCI_NUMBER', n, fibonacci(n));
+    }, 1000);
+  });
 }
 
 A(10);
