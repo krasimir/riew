18eadef507f4b4913ec42933dddeef0cc51f5c36
Krasimir Tsonev <krasimir@outset.ws>
2020-01-12 07:56:33 +0200

    Example

--- examples/snippets/code.js
+++ examples/snippets/code.js
@@ -1,8 +1,10 @@
-import { chan, buffer, sput, sread } from 'riew';
-
-global.__DEV__ = true;
+import { chan, buffer, sput, sread, go, put } from 'riew';
 
 const ch = chan(buffer.memory());
 
-sread(ch, v => console.log(v));
+sread(ch, v => console.log(v), { listen: true });
 sput(ch, 'foo', () => console.log('Yeah'));
+
+go(function*() {
+  yield put(ch, 'bar');
+});
